000bd0c4c4bcdfd91720a74718aa1fdc75a6a92a
Tanvirahmed1234567 <dipto123132@gmail.com>
2024-09-11 19:06:00 +0600

    Update npx11.js

--- scripts/commands/npx11.js
+++ scripts/commands/npx11.js
@@ -1,37 +1,37 @@
 const fs = require("fs");
 module.exports = {
   config:{
-  name: "npx11",
+	name: "npx11",
         version: "1.0.1",
         prefix: false,
-  permssion: 0,
-  credits: "nayan", 
-  description: "Fun",
-  category: "no prefix",
-  usages: "😒",
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "😒",
         cooldowns: 5, 
 },
 
 handleEvent: async function({ api, event, client, __GLOBAL }) {
-  var { threadID, messageID } = event;
+	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/rztWmL1.mp4','https://i.imgur.com/nR7U3mO.mp4','https://i.imgur.com/giMsMV5.mp4','https://i.imgur.com/8PQrQmR.mp4',
+      'https://i.imgur.com/h1ShDPM.mp4','https://i.imgur.com/42m1r5V.mp4',
       { responseType: 'stream' }
     )
   ).data;
 
-  if (body.indexOf("🤦🏼‍♀️")==0 || body.indexOf("🤷🏼‍♀️")==0 || body.indexOf("🙆🏼‍♀️")==0 || body.indexOf("🏃🏼‍♀️")==0 || body.indexOf("💁🏼‍♀️")==0 || body.indexOf("kalifa")==0 || body.indexOf("👩‍❤️‍💋‍👩")==0 || body.indexOf("Mia khalia")==0 || body.indexOf("💃")==0 || body.indexOf("👰‍♂️")==0) {
-    var msg = {
-        body: " 👅-•|•Afa nkî_//-💃🥹 ",
-        attachment: media
-      }
-      api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🫣",event.messageID, (err) => {}, true)
-    }
-  },
-  start: function({ nayan }) {
+	if (body.indexOf("👀")==0 || body.indexOf("👀👀")==0 || body.indexOf("👁️")==0 || body.indexOf("👁️👁️")==0 || body.indexOf("")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("")==0) {
+		var msg = {
+				body: " -•|•yØur eyes_//-🫵🏻👀 ",
+				attachment: media
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("😌",event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
   }
